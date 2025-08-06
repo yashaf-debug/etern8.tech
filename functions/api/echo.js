3c43a72d273b4ca8b8a13f1cd3e6821eaf03cef8
yashaf-debug <radchenko.yakov@gmail.com>
2025-08-06 23:09:18 +0300

    Create echo.js

---
+++ functions/api/echo.js
@@ -0,0 +1,6 @@
+export function onRequestGet() {
+  return new Response(JSON.stringify({ ok: true, ts: Date.now() }), {
+    status: 200,
+    headers: { 'content-type': 'application/json; charset=utf-8' }
+  });
+}
